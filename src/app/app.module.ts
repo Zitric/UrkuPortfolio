323ea66c66fcfee9fc38a8934e06ccc6ef38c5d0
Zitric <zitricwebdeveloper@gmail.com>
2018-03-22 00:43:05 +0100

    I started with the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
